fc247ac9a265f85dde1eccd47bf804d193402903
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-05-10 17:28:04 -0500

    keeps making temporary .py files, continuously delete. edited serializer file.

--- music/views.py
+++ music/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Music
+from .serializer import Musicserializer
+
 
 @api_view(['GET'])
 def music_list(request):
+    music = Music.objects.all()
 
+    serializer = Musicserializer(music, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
