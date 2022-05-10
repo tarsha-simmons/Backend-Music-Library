7d820f1df0359d8b719d4e536a760e374c3a2e6f
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-05-10 14:42:31 -0500

    fixed files. startapp

--- music_project/settings.py
+++ music_project/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.corsheaders',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
