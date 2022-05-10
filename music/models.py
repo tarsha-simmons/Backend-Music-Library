6112293512cff1273c725c3375d0658bc70d258e
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-05-10 15:21:43 -0500

    created model for app, registered rest framework in project settings installed apps

--- music/models.py
+++ music/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Music(models.Model):
+    title = models.CharField(max_length=250)
+    artist = models.CharField(max_length=250)
+    album = models.CharField(max_length=250)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=300)
+    likes = models.IntegerField(default=0)
+    album_art = models.CharField(max_length=1000)
